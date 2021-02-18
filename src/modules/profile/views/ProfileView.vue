b48a767dbd7677443337e2d37a26db20841653a7
Nelkor <nelkor9855118751@gmail.com>
2021-02-18 08:35:47 +0300

    wip

--- src/modules/profile/views/ProfileView.vue
+++ src/modules/profile/views/ProfileView.vue
@@ -1,9 +1,30 @@
 <template>
-  <div class="profile-view"></div>
+  <div class="profile-view">
+    <div
+      v-if="isOwnerId"
+      class="log-out"
+    >
+      <div class="head">Выйти из профиля</div>
+
+      <button>На этом устройстве</button>
+
+      <button>На всех устройствах</button>
+
+      <button>На остальных устройствах</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'ProfileView',
+  computed: {
+    profileId() {
+      return this.$route.params.id
+    },
+    isOwnerId() {
+      return Number(this.profileId) === this.$store.state.auth.id
+    },
+  },
 }
 </script>
